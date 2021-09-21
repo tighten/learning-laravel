1536c1178ccd8e61a42e0fa741b218987e1cc353
Kristin Collins <kristin@tighten.co>
2021-09-21 17:05:05 -0400

    Install dependencies.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    colors: {
+      brand: '#bf201d',
+      light: '#f4f2f1',
+      lightAccent: '#d67621',
+      dark: '#b58b7f',
+      darkAccent: '#34283e',
+      primary: '#bf201d',
+      info: '#33283e',
+      success: '#6f8441',
+      warning: '#ec7409',
+      danger: '#f44336',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
