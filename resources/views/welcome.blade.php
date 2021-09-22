e2ab4f72c5c3e0d2bb6060fa91dacd22ea5ea18e
Kristin Collins <kristin@tighten.co>
2021-09-22 15:26:30 -0400

    Add breeze and welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,13 @@
 @extends('_layouts.base')
 
 @section('content')
-    <div class="container">
-        <h1>Welcome Player</h1>
-        <p>Can't catch me.</p>
+    <div class="h-screen flex justify-center items-center">
+        <div>
+            <h1 class="text-7xl text-center">Ready Player?</h1>
+            <div class="my-5 grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+                <a href="/login" class="button button-primary">Log In</a>
+                <a href="/register" class="button button-outline">Sign Up</a>
+            </div>
+        </div>
     </div>
 @endsection
