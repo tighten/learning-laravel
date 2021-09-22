4b0969bd0f25fc7bc97966510b4b9877308fd10b
Kristin Collins <kristin@tighten.co>
2021-09-22 16:59:11 -0400

    Add images for verify email view.

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -19,7 +19,7 @@
         <div class="mt-4 flex items-center justify-between">
             <form method="POST" action="{{ route('verification.send') }}">
                 @csrf
-
+`
                 <div>
                     <x-button>
                         {{ __('Resend Verification Email') }}
