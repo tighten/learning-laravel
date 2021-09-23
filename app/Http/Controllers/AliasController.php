6909fe1317e7d83a5a238633d4c577a9528f35b9
Kristin Collins <kristin@tighten.co>
2021-09-23 14:11:41 -0400

    Add infrastructure for adding user aliases to the database.

---
+++ app/Http/Controllers/AliasController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alias;
+use Illuminate\Http\Request;
+
+class AliasController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Alias $alias)
+    {
+        //
+    }
+
+    public function edit(Alias $alias)
+    {
+        //
+    }
+
+    public function update(Request $request, Alias $alias)
+    {
+        //
+    }
+
+    public function destroy(Alias $alias)
+    {
+        //
+    }
+}
