9f3a2cefb6475ad406dd3a71a77f0eb0b4077e41
Kristin Collins <kristin@tighten.co>
2021-09-23 12:30:18 -0400

    Update styles for dashboard display.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-3xl">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
+    <div class="py-6">
+        <div class="sm:px-6 lg:px-8">
+            <p>You're logged in!</p>
         </div>
     </div>
 </x-app-layout>
