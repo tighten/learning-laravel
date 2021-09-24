b41cc8946b0c7c678564eddfde793bb0954a58bc
Kristin Collins <kristin@tighten.co>
2021-09-24 10:08:27 -0400

    Add pagination links to table component.

--- resources/views/resource/index.blade.php
+++ resources/views/resource/index.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-6">
         <div class="sm:px-6 lg:px-8">
-            <x-table :name="$name" :param="$param" :headers="$headers" :fields="$fields" />
+            <x-table :name="$name" :param="$param" :headers="$headers" :fields="$fields" :rows="$rows" />
         </div>
     </div>
 </x-app-layout>
