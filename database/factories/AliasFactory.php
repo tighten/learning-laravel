c80d401e2f5d479942537f0586df71a393b15fe0
Kristin Collins <kristin@tighten.co>
2021-09-27 17:34:22 -0400

    Add alias factory and seeder.

---
+++ database/factories/AliasFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Alias;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AliasFactory extends Factory
+{
+    protected $model = Alias::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory()->make()->id,
+            'name' => $this->faker->name(),
+            'email' => $this->faker->email(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'cc_type' => $this->faker->creditCardType(),
+            'cc_number' => $this->faker->creditCardNumber(),
+            'cc_expiration' => $this->faker->creditCardExpirationDate(),
+            'company' => $this->faker->company(),
+            'job_title' => $this->faker->jobTitle(),
+            'company_email' => $this->faker->companyEmail(),
+            'username' => $this->faker->userName(),
+            'password' => $this->faker->password(),
+            'ip_address' => $this->faker->ipv4(),
+            'mac_address' => $this->faker->macAddress(),
+            'profile' => $this->faker->realText(500),
+            'burned' => $this->faker->boolean(),
+        ];
+    }
+}
