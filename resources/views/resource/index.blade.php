78929059857808e51e0f8380a7ed51718f07594c
Kristin Collins <kristin@tighten.co>
2021-09-28 09:12:39 -0400

    Add alias generate button and method.

--- resources/views/resource/index.blade.php
+++ resources/views/resource/index.blade.php
@@ -5,6 +5,12 @@
         </h2>
     </x-slot>
 
+    <div class="mx-8 my-5">
+        <a href="{{ route('aliases.create') }}" class="button button-primary">
+            Generate an Alias
+        </a>
+    </div>
+
     <div class="py-6">
         <div class="sm:px-6 lg:px-8">
             <x-table :name="$name" :param="$param" :headers="$headers" :fields="$fields" :rows="$rows" />
