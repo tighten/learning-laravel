fd2e9dabe4f2dd555aa65daf7ad37cb20684f35a
Shift <shift@laravelshift.com>
2022-02-04 15:26:06 +0000

    Adopt anonymous migrations

--- database/migrations/2021_09_23_171202_create_aliases_table.php
+++ database/migrations/2021_09_23_171202_create_aliases_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAliasesTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -36,4 +36,4 @@ class CreateAliasesTable extends Migration
     {
         Schema::dropIfExists('aliases');
     }
-}
+};
